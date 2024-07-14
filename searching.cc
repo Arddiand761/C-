9b3965caea3420e2a0a9b8f4ad8e232e6301254a
Arddian Darmawan_2200018157 <128204076+Arddiand761@users.noreply.github.com>
2024-07-15 01:38:48 +0700

    searching.cc

--- searching.cc
+++ searching.cc
@@ -4,28 +4,27 @@ using namespace std;
 
 
 
-
 int search(int arr[],int size, int target){
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)//looping for searching target on array
     {
-        if (arr[i] == target)
+        if (arr[i] == target)// to check is the number at the index of array same as the target
         {
-            return i;
+            return i;//target found
         }else{
             
         }
         
     }
-    return -1;
+    return -1;//target not found
 }
 
 int main(){
-    int data[]={1,2,3,45,6,7};
-    int size = sizeof(data)/sizeof(data[0]);
+    int data[]={1,2,3,45,6,7};//data of number
+    int size = sizeof(data)/sizeof(data[0]);//Used to calculate the total size of the data array and the size of a single element of the array.
     int target;
 
 
-    cout<<"Enter the number :";
+    cout<<"Enter the number :";//enter the number you want to find
     cin>>target;
 
     int result = search(data, size, target);
